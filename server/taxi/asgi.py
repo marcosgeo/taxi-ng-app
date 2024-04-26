baab6b4a6772657025f66e11232685ce27c52bfb
Antonio Marcos <marcosgeo@yahoo.com>
2024-04-26 08:18:03 -0300

    finished web-socket connections authentication

--- server/taxi/asgi.py
+++ server/taxi/asgi.py
@@ -18,11 +18,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi.settings')
 
 django_asgi_application = get_asgi_application()
 
+from taxi.middleware import TokenAuthMiddlewareStack
 from trips.consumers import TaxiConsumer
 
 application = ProtocolTypeRouter({
     'http': django_asgi_application,
-    'websocket': URLRouter([
-        path('taxi/', TaxiConsumer.as_asgi())
-    ])
+    'websocket': TokenAuthMiddlewareStack(
+        URLRouter([
+            path('taxi/', TaxiConsumer.as_asgi()),
+        ])
+    ),
 })
