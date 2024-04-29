478cf33422fa45bdfd712d97660f464d3671840c
Antonio Marcos <marcosgeo@yahoo.com>
2024-04-29 07:56:32 -0300

    finished join driver to a pool

--- server/trips/consumers.py
+++ server/trips/consumers.py
@@ -1,7 +1,12 @@
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
+    @database_sync_to_async
+    def _get_user_group(self, user):
+        return user.groups.first().name
+
     groups = ['test']
 
     async def connect(self):
@@ -9,11 +14,23 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
         if user.is_anonymous:
             await self.close()
         else:
-            await self.channel_layer.group_add(group='test', channel=self.channel_name)
+            user_group = await self._get_user_group(user)
+            if user_group == 'driver':
+                await self.channel_layer.group_add(
+                    group='drivers', channel=self.channel_name
+                )
             await self.accept()
 
     async def disconnect(self, code):
-        await self.channel_layer.group_discard(group='test', channel=self.channel_name)
+        user = self.scope['user']
+        if user.is_anonymous:
+            await self.close()
+        else:
+            user_group = await self._get_user_group(user)
+            if user_group == 'driver':
+                await self.channel_layer.group_discard(
+                    group='drivers', channel=self.channel_name
+                )
         await super().disconnect(code)
 
     async def echo_message(self, message):
